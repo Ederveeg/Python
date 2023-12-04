6d66f7b7668da0495a7f6e059dca1d0ff2d9bd37
Eder Vega <aldhaveg01@gmail.com>
2023-12-04 14:11:00 -0600

    Creamos programa donde practicamos el cambio o transformacion de tipos de objetos en py

---
+++ Transformacion.py
@@ -0,0 +1,19 @@
+name = "Eder"
+print(type(name))
+name = 12
+print(type(name))
+name = True
+print(type(name))
+
+print ("Eder" + " Vega")
+print(10 + 20)
+print("Eder" + " 19")
+
+age = 19
+print("Mi edad es " + str(age))
+
+age = input('Escribe tu edad = ')
+print(type(age))
+age = int(age)
+age += 10
+print(f'Tu edad en 10 años sera {age}')
