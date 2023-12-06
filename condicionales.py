73355835276e3717bf46e88a7c2b01bad64332af
Eder Vega <aldhaveg01@gmail.com>
2023-12-05 21:58:50 -0600

    Agregamos proyecto de condicionales

--- condicionales.py
+++ condicionales.py
@@ -17,11 +17,10 @@ else:
     print('No tienes ninguna mascota interessante')
 
 
-'''
+
 stock = int(input('Digita el stock => '))
 
 if stock >= 100 and stock <= 1000:
     print('El stock es correcto')
 else:
     print('El stock es incorrecto')
-'''
