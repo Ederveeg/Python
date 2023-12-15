b93fa71205a9cbafcf3d56bf04976fdb15cef805
Eder Vega <aldhaveg01@gmail.com>
2023-12-15 11:47:03 -0600

    Se añadio archivo de ciclos anidados y se modifico juego de piedra papel y tijera

--- proyecto_juego.py
+++ proyecto_juego.py
@@ -3,17 +3,43 @@
 import random
 options = ('piedra', 'papel', 'tijera')
 
-#Declarar 2 variables: jugador y pc
-jugador = input('piedra, papel o tijera: ').lower().strip()
-pc = random.choice(options)
+computer_wins = 0
+user_wins = 0
+rounds = 1
 
-print('El jugador escogio: ', jugador)
-print('El pc escogio: ', pc)
+while True:
 
-#Condicionales
-if jugador == pc:
-    print('¡Empate!')
-elif (jugador == 'piedra' and pc == 'tijera') or (jugador == 'papel' and pc == 'piedra') or (jugador == 'tijera' and pc == 'papel'):
-    print('¡Ganaste!')
-else:
-    print('Perdiste')
+    print('*' * 10 )
+    print('ROUND', rounds)
+    print('*' * 10)
+
+    #Declarar 2 variables: jugador y pc
+    jugador = input('piedra, papel o tijera: ').lower().strip()
+    pc = random.choice(options)
+
+    print('El jugador escogio: ', jugador)
+    print('El pc escogio: ', pc)
+
+    #Condicionales
+    if jugador == pc:
+        print('¡Empate!')
+    elif (jugador == 'piedra' and pc == 'tijera') or (jugador == 'papel' and pc == 'piedra') or (jugador == 'tijera' and pc == 'papel'):
+        print('¡Ganaste!')
+        user_wins += 1
+    else:
+        print('Perdiste')
+        computer_wins += 1
+
+    if computer_wins == 2:
+        print('¡El computador gano!')
+        break
+
+    if user_wins == 2:
+        print('¡Ganaste la partida!')
+        break
+
+    rounds += 1
+
+
+
+
