c23dedfb7a8c8308395e8c45e4f75e68af4f434b
Eder Vega <aldhaveg01@gmail.com>
2023-12-15 19:02:50 -0600

    Elimine archivo por falta de contenido

--- proyecto_condicionales.py
+++
@@ -1,9 +0,0 @@
-#Proyecto de creacion de programa que evalue si un numero es par o impar
-
-#variable numero con la cual trabajaremos para obetener su dato de entrada
-numero = int(input('Ingresa un numero: '))
-
-if numero % 2 == 0:
-    print('Tu numero es par')
-else:
-    print('Tu numero es impar')
