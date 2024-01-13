21e20b7200c0d289f4f3f1cd67dee7d6db1184ff
Eder Vega <aldhaveg01@gmail.com>
2024-01-13 12:24:20 -0600

    Añadi funciones y map con diccionarios

--- dicc.py
+++ dicc.py
@@ -120,16 +120,43 @@ result = { country: population for (country, population) in population_v2.items(
 print(result)
 '''
 
-#Programa
-
+#Programa que cuenta las vocales
+'''
 text = 'Hola soy Eder'
 unique = { c.upper() : c.count(c)  for c in text if c in 'aeiou'}
 print(unique)
+'''
 
+#Map con diccionarios
 
+items = [
+    {
+        'product': 'camisa',
+        'price': 100
+    },
+    {
+        'product': 'pantalones',
+        'price': 300
+    },
+    {
+        'product': 'pantalones 2',
+        'price': 200
+    }
+]
 
+prices = list(map(lambda item: item['price'], items ))
+print(prices)
 
+def add_taxes(item):
+    new_item = item.copy()
+    new_item['taxes'] = new_item['price'] * .19
+    return item
 
+new_items = list(map(add_taxes, items))
+print('new list')
+print(new_items)
+print('old list')
+print(items)
 
 
 
--- funciones.py
+++ funciones.py
@@ -131,7 +131,7 @@ print(text)
 '''
 
 #HOF: Higher order function
-
+'''
 def increment(x):
     return x + 1
 
@@ -148,6 +148,56 @@ print(result)
 
 result = hof_v2(2, increment_v2)
 print(result)
+result = hof_v2(2, lambda x: x * 2)
+'''
+
+#Map/transformacion de elementos y es de los elementos mas usados
+'''
+numbers = [1,2,3,4]
+numbers_v2 = []
+for i in numbers:
+    numbers_v2.append(i * 2)
+
+#Aqui usamos map
+numbers_v3 = list(map(lambda i: i * 2, numbers))
+
+print(numbers)
+print(numbers_v2)
+print(numbers_v3)
+
+numbers_1 = [1, 2, 3, 4]
+numbers_2 = [5, 6, 7]
+
+print(numbers_1)
+print(numbers_2)
+result = list(map(lambda x, y: x + y, numbers_1, numbers_2))
+print(result)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
